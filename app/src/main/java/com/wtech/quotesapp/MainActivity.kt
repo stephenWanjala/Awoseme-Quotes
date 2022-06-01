499f8f1b3766b597525837e35b2450972569d21b
wanjala stephen <stephenwanjala145@gmail.com>
2022-06-01 05:16:35 +0300

    copy to clipBoard

--- app/src/main/java/com/wtech/quotesapp/MainActivity.kt
+++ app/src/main/java/com/wtech/quotesapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.wtech.quotesapp
 
 import android.app.ProgressDialog
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -24,7 +26,8 @@ class MainActivity : AppCompatActivity() {
     private val listener=object :QuotesResponseListener{
         override fun didAfetch(response: List<QuotesResponse>, mesaage: String) {
            progressBarDialog.dismiss()
-            val recyclerViewAdapter=QuotesListAdapter(this@MainActivity,response)
+            val recyclerViewAdapter=QuotesListAdapter(this@MainActivity,response,
+            copyQuoteListener)
             binding.recyclerView.apply {
                 setHasFixedSize(true)
                 layoutManager=StaggeredGridLayoutManager(2,LinearLayoutManager.VERTICAL)
@@ -38,6 +41,18 @@ class MainActivity : AppCompatActivity() {
             progressBarDialog.dismiss()
             Toast.makeText(this@MainActivity,
             message,Toast.LENGTH_LONG)
+                .show()
+        }
+
+    }
+
+    private  val copyQuoteListener=object:CopyQuoteListener{
+        override fun onQuoteClicked(text: String) {
+            val clipBoard=getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+            val clip=ClipData.newPlainText("copied Quote",text)
+            clipBoard.setPrimaryClip(clip)
+            Toast.makeText(this@MainActivity
+            ,"Qoute Copied to clipBoard",Toast.LENGTH_LONG)
                 .show()
         }
 
